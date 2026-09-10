b462e9afb0a0849127c6b54f7c9c15ab2e751272
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-10 09:44:17 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿string[] englishWords = File.ReadAllLines("english.txt");
+string[] portugueseWords = File.ReadAllLines("portuguese.txt");
+
+System.Console.WriteLine(portugueseWords[80] + "\t" + englishWords[80]);
