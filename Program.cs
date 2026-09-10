1d8d888ccfbf8483eebb4d923fecf608ea38af9f
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-10 15:44:50 +0200

    Added classes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿string[] englishWords = File.ReadAllLines("english.txt");
 string[] portugueseWords = File.ReadAllLines("portuguese.txt");
+int[] score1 = new int[englishWords.Length];
+int[] score2 = new int[englishWords.Length];
+int[] score3 = new int[englishWords.Length];
+int[] score4 = new int[englishWords.Length];
+int[] score5 = new int[englishWords.Length];
+
+
 
 System.Console.WriteLine(portugueseWords[80] + "\t" + englishWords[80]);
+
+public void initializeAll()
+{
+    
+}
