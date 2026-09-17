33b4a3f24233f92a64c1bf5de67e269af1f07fce
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-17 14:46:22 +0200

    Added more txt files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
-﻿string[] englishWords = File.ReadAllLines("english.txt");
+﻿using System.Security.Cryptography;
+
+string[] englishWords = File.ReadAllLines("english.txt");
 string[] portugueseWords = File.ReadAllLines("portuguese.txt");
+string[] ele = File.ReadAllLines("ele_ela.txt");
+string[] eles = File.ReadAllLines("eles_elas.txt");
+string[] eu = File.ReadAllLines("eu.txt");
+string[] nos = File.ReadAllLines("nos.txt");
+string[] voce = File.ReadAllLines("voce.txt");
+string[] voces = File.ReadAllLines("voces.txt");
+
+string[] eleEn = File.ReadAllLines("ele_ela_en.txt");
+string[] elesEn = File.ReadAllLines("eles_elas_en.txt");
+string[] euEn = File.ReadAllLines("eu_en.txt");
+string[] nosEn = File.ReadAllLines("nos_en.txt");
+string[] voceEn = File.ReadAllLines("voce_en.txt");
+string[] vocesEn = File.ReadAllLines("voces_en.txt");
+
+string[] formatPT = {"Eu","Você", "Ele", "Ela", "Eles", "Elas", "Nos", "Vocês"};
+string[] formatEN = {"I","You (singular)", "He", "She", "They", "They (females)", "We", "You (plural)"};
+// formatPT[0] = Eu
+// formatPT[0] = Você
+// formatPT[0] = Ele
+// formatPT[0] = Ela
+// formatPT[0] = Eles
+// formatPT[0] = Elas
+// formatPT[0] = Nos
+// formatPT[0] = Vocês
+
+
 int[] score1 = new int[englishWords.Length];
 int[] score2 = new int[englishWords.Length];
 int[] score3 = new int[englishWords.Length];
@@ -8,9 +36,9 @@ int[] score5 = new int[englishWords.Length];
 List<Verb> allVerbs = new List<Verb>();
 Initialize();
 
-foreach (var v in allVerbs)
+for (int i = 0; i < englishWords.Length ; i++)
 {
-    Console.WriteLine(v);
+    
 }
 
 
@@ -25,8 +53,6 @@ void Initialize()
         score3[i] = 0 ;
         score4[i] = 0 ;
         score5[i] = 0 ;
-
-        Verb verb = new Verb(portugueseWords[i], englishWords[i]);
-        allVerbs.Add(verb);
+        
     }
 }
